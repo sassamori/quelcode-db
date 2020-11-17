bc7de22dd31526ca7d404a1901fa175eb85cdeb2
sassamori <mtkmnaerkn@yahoo.co.jp>
2020-11-17 17:07:46 +0900

    db-001.sqlの記述

---
+++ db/db-challenge3/db-001/db-001.sql
@@ -0,0 +1,15 @@
+INSERT INTO users
+(mail,password,name,introduction,work_phone_number,cell_phone_number,is_deleted,created_at,updated_at)
+VALUES(
+('admin@labot.inc','admin','admin','このスペースの管理者です。','00-0000-0000',null,0,'2020/04/20 15:47:52','2020/04/20 15:47:52'),
+('mysql@labot.inc','mysql55','mysql',null,null,'080-1234-1253',0,'2020/04/22 16:02:55','2020/04/22 16:02:55'),
+('sqlserver@labot.inc','apple','sqlserver','オラクルデータベースが好きです。',null,null,0,'2020/04/22 17:32:55','2020/04/22 17:32:55'),
+('mariadb@labot.inc','maria','mariadb',null,'03-6666-5879',null,0,'2020/04/22 18:45:21','2020/04/22 18:45:21'),
+('laravel@labot.inc','lara','laravel',null,null,null,0,'2020/04/22 18:46:01','2020/04/22 18:46:01'),
+('cakephp@labot.inc','cake89','cake',null,'03-987-1234','090-5555-7854',0,'2020/04/23 02:51:06','2020/04/25 11:45:06'),
+('react@labot.inc','node','react',null,null,null,0,'2020/04/23 09:54:32','2020/04/23 09:54:32'),
+('html@labot.inc','css3','html5',null,null,'080-5849-5555',0,'2020/04/23 09:55:14','2020/04/23 09:55:14'),
+('php@labot.inc','php7','php','伝説のphp6',null,null,0,'2020/04/24 01:01:06','2020/04/24 01:01:06'),
+('javascript@labot.inc','react','js','Hello world','0120-555-458',null,0,'2020/04/24 05:03:58','2020/04/24 05:03:58'),
+('microsoft@gmail.com','google','amazon','facebook',null,'080-9999-5555',1,'2020/04/24 06:00:02','2020/04/26 12:13:43')
+);
