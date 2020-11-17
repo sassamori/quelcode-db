a518c912666d2e82f48bd23aabc602f67a5750d0
sassamori <mtkmnaerkn@yahoo.co.jp>
2020-11-17 18:13:31 +0900

    カラムの順番を入れ替え

--- db/db-challenge3/db-002/db-002.sql
+++ db/db-challenge3/db-002/db-002.sql
@@ -1,5 +1,5 @@
 INSERT INTO rooms
-(name,summary,is_enabled_to_upload,is_direct_chat,is_deleted,created_at,create_user_id,updated_at,update_user_id)
+(name,summary,is_enabled_to_upload,is_direct_chat,is_deleted,create_user_id,created_at,update_user_id,updated_at)
 VALUES
 ('りんご','利用者の集まり',1,0,0,1,'2020/04/21 11:26:32',1,'2020/04/21 11:26:32'),
 ('ごりら','sqlの集まり',1,0,0,2,'2020/04/23 15:42:05',2,'2020/04/23 15:42:05'),
