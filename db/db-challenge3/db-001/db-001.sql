f0bc133e8d0f623fe2169d1e95115a842297a333
sassamori <mtkmnaerkn@yahoo.co.jp>
2020-11-17 17:35:45 +0900

    VALUESの後のカッコを削除

--- db/db-challenge3/db-001/db-001.sql
+++ db/db-challenge3/db-001/db-001.sql
@@ -1,15 +1,15 @@
 INSERT INTO users
 (id,mail,password,name,introduction,work_phone_number,cell_phone_number,is_deleted,created_at,updated_at)
-VALUES(
-(0,'admin@labot.inc','admin','admin','このスペースの管理者です。','00-0000-0000',null,0,'2020/04/20 15:47:52','2020/04/20 15:47:52'),
-(0,'mysql@labot.inc','mysql55','mysql',null,null,'080-1234-1253',0,'2020/04/22 16:02:55','2020/04/22 16:02:55'),
-(0,'sqlserver@labot.inc','apple','sqlserver','オラクルデータベースが好きです。',null,null,0,'2020/04/22 17:32:55','2020/04/22 17:32:55'),
-(0,'mariadb@labot.inc','maria','mariadb',null,'03-6666-5879',null,0,'2020/04/22 18:45:21','2020/04/22 18:45:21'),
-(0,'laravel@labot.inc','lara','laravel',null,null,null,0,'2020/04/22 18:46:01','2020/04/22 18:46:01'),
-(0,'cakephp@labot.inc','cake89','cake',null,'03-987-1234','090-5555-7854',0,'2020/04/23 02:51:06','2020/04/25 11:45:06'),
-(0,'react@labot.inc','node','react',null,null,null,0,'2020/04/23 09:54:32','2020/04/23 09:54:32'),
-(0,'html@labot.inc','css3','html5',null,null,'080-5849-5555',0,'2020/04/23 09:55:14','2020/04/23 09:55:14'),
-(0,'php@labot.inc','php7','php','伝説のphp6',null,null,0,'2020/04/24 01:01:06','2020/04/24 01:01:06'),
-(0,'javascript@labot.inc','react','js','Hello world','0120-555-458',null,0,'2020/04/24 05:03:58','2020/04/24 05:03:58'),
-(0,'microsoft@gmail.com','google','amazon','facebook',null,'080-9999-5555',1,'2020/04/24 06:00:02','2020/04/26 12:13:43')
-);
+VALUES
+(1,'admin@labot.inc','admin','admin','このスペースの管理者です。','00-0000-0000',null,0,'2020/04/20 15:47:52','2020/04/20 15:47:52'),
+(2,'mysql@labot.inc','mysql55','mysql',null,null,'080-1234-1253',0,'2020/04/22 16:02:55','2020/04/22 16:02:55'),
+(3,'sqlserver@labot.inc','apple','sqlserver','オラクルデータベースが好きです。',null,null,0,'2020/04/22 17:32:55','2020/04/22 17:32:55'),
+(4,'mariadb@labot.inc','maria','mariadb',null,'03-6666-5879',null,0,'2020/04/22 18:45:21','2020/04/22 18:45:21'),
+(5,'laravel@labot.inc','lara','laravel',null,null,null,0,'2020/04/22 18:46:01','2020/04/22 18:46:01'),
+(6,'cakephp@labot.inc','cake89','cake',null,'03-987-1234','090-5555-7854',0,'2020/04/23 02:51:06','2020/04/25 11:45:06'),
+(7,'react@labot.inc','node','react',null,null,null,0,'2020/04/23 09:54:32','2020/04/23 09:54:32'),
+(8,'html@labot.inc','css3','html5',null,null,'080-5849-5555',0,'2020/04/23 09:55:14','2020/04/23 09:55:14'),
+(9,'php@labot.inc','php7','php','伝説のphp6',null,null,0,'2020/04/24 01:01:06','2020/04/24 01:01:06'),
+(10,'javascript@labot.inc','react','js','Hello world','0120-555-458',null,0,'2020/04/24 05:03:58','2020/04/24 05:03:58'),
+(11,'microsoft@gmail.com','google','amazon','facebook',null,'080-9999-5555',1,'2020/04/24 06:00:02','2020/04/26 12:13:43')
+;
