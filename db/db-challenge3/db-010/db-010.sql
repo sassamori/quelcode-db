0e0cd38e937042d76265744072b3fa2c25751ca2
sassamori <mtkmnaerkn@yahoo.co.jp>
2020-11-23 16:45:34 +0900

    db-010.sqlの記述3

--- db/db-challenge3/db-010/db-010.sql
+++ db/db-challenge3/db-010/db-010.sql
@@ -4,7 +4,7 @@ LEFT JOIN users
 ON posts1.update_user_id = users.id
 LEFT JOIN rooms
 ON posts1.room_id = rooms.id
-WHERE users.is_deleted = 0 AND rooms.is_deleted = 0 AND posts1.update_user_id = (
+WHERE users.is_deleted = 0 AND rooms.is_deleted = 0 AND posts1.is_deleted = 0 AND posts1.update_user_id = (
     SELECT posts2.update_user_id
     FROM posts AS posts2 
     WHERE posts1.room_id = posts2.room_id
