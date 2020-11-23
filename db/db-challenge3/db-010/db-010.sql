ea2c39dcba100c452a9eadd8941a0b70d7632d0e
sassamori <mtkmnaerkn@yahoo.co.jp>
2020-11-23 16:50:41 +0900

    db-010.sqlの記述

--- db/db-challenge3/db-010/db-010.sql
+++ db/db-challenge3/db-010/db-010.sql
@@ -4,10 +4,10 @@ LEFT JOIN users
 ON posts1.update_user_id = users.id
 LEFT JOIN rooms
 ON posts1.room_id = rooms.id
-WHERE users.is_deleted = 0 AND rooms.is_deleted = 0 AND posts1.is_deleted = 0 AND posts1.update_user_id = (
+WHERE users.is_deleted = 0 AND posts1.is_deleted = 0 AND posts1.update_user_id = (
     SELECT posts2.update_user_id
     FROM posts AS posts2 
-    WHERE posts1.room_id = posts2.room_id
+    WHERE posts1.room_id = posts2.room_id AND posts2.is_deleted = 0
     ORDER BY posts2.updated_at DESC
     LIMIT 1
 )
