f3037e5ab857e706928426289dfca41bb98dcc61
sassamori <mtkmnaerkn@yahoo.co.jp>
2020-11-23 16:27:53 +0900

    db-010.sqlの記述

---
+++ db/db-challenge3/db-010/db-010.sql
@@ -0,0 +1,14 @@
+SELECT posts1.update_user_id,users.name,posts1.update_at,posts1.room_id,rooms.name
+FROM posts AS posts1
+LEFT JOIN users
+ON posts1.update_user_id = users.id
+LEFT JOIN rooms
+ON posts1.room_id = rooms.id
+WHERE users.is_deleted = 0 AND rooms.is_deleted = 0 AND posts1.update_user_id = (
+    SELECT posts2.update_user_id
+    FROM posts AS posts2 
+    WHERE posts1.room_id = posts2.room_id
+    ORDER BY posts2.update_at DESC
+    LIMIT 1
+)
+ORDER BY posts1.room_id ASC;
