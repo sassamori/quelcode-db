cf4b43b685807c404e6bb8f215e7b58996386398
sassamori <mtkmnaerkn@yahoo.co.jp>
2020-11-25 16:46:33 +0900

    db-010.sqlのupdate系をpost系に修正

--- db/db-challenge3/db-010/db-010.sql
+++ db/db-challenge3/db-010/db-010.sql
@@ -1,14 +1,14 @@
-SELECT posts1.update_user_id,users.name AS ユーザー名,posts1.updated_at,posts1.room_id,rooms.name AS ルーム名
+SELECT posts1.post_user_id,users.name AS ユーザー名,posts1.posted_at,posts1.room_id,rooms.name AS ルーム名
 FROM posts AS posts1
 LEFT JOIN users
-ON posts1.update_user_id = users.id
+ON posts1.post_user_id = users.id
 LEFT JOIN rooms
 ON posts1.room_id = rooms.id
-WHERE users.is_deleted = 0 AND posts1.is_deleted = 0 AND posts1.update_user_id = (
-    SELECT posts2.update_user_id
+WHERE users.is_deleted = 0 AND posts1.is_deleted = 0 AND posts1.post_user_id = (
+    SELECT posts2.post_user_id
     FROM posts AS posts2 
     WHERE posts1.room_id = posts2.room_id AND posts2.is_deleted = 0
-    ORDER BY posts2.updated_at DESC
+    ORDER BY posts2.posted_at DESC
     LIMIT 1
 )
 ORDER BY posts1.room_id ASC;
