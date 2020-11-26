8731862be801722809e34d08ac15d6182b6449ae
sassamori <mtkmnaerkn@yahoo.co.jp>
2020-11-27 06:42:54 +0900

    db-009.sqlの修正とキャプチャの追加

--- db/db-challenge3/db-009/db-009.sql
+++ db/db-challenge3/db-009/db-009.sql
@@ -1,9 +1,9 @@
-SELECT rooms.name AS チャットルーム名,COUNT(*) AS 投稿数
+SELECT posts.room_id,rooms.name AS チャットルーム名,COUNT(*) AS 投稿数
 FROM posts
 LEFT JOIN rooms
 ON posts.room_id = rooms.id
 LEFT JOIN users
 ON posts.post_user_id = users.id
 WHERE posts.is_deleted = 0 AND users.is_deleted = 0
-GROUP BY rooms.name
+GROUP BY posts.room_id
 ORDER BY 投稿数 DESC;
