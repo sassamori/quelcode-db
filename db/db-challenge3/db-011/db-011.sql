7e59c6b05ce1313f810d51d21f482289eb4a4939
sassamori <mtkmnaerkn@yahoo.co.jp>
2020-11-28 11:08:08 +0900

    db-011.sqlの記述2

--- db/db-challenge3/db-011/db-011.sql
+++ db/db-challenge3/db-011/db-011.sql
@@ -1,6 +1,6 @@
 BEGIN;
 UPDATE tasks
 SET is_done = 1,update_user_id = 1,updated_at = NOW()
-WHERE created_at BETWEEN '2020-04-26 09:00' AND '2020-04-26 11:30' 
+WHERE created_at BETWEEN '2020-04-26 09:00:00' AND '2020-04-26 11:30:00'
     AND is_deleted = 0
 COMMIT;
